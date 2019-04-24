9ab18a40d14b3eee78414e75b84319bdd64c73b9
CMS <sirkku.heija@gmail.com>
2019-04-24 11:16:48 +0300

    Added gitingnore

--- surveys/models.py
+++ surveys/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Survey (models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=1000)
+    date_created = models.DateTimeField()
+    def __str__(self):
+        return self.title
+
+class Question (models.Model):
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
+    def __str__(self):
+            return self.question_text
+
+class Answer (models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=200)
+    def __str__(self):
+            return self.answer_text
