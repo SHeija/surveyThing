e247068040df3c509826841be0295c7b850952a6
CMS <sirkku.heija@gmail.com>
2019-04-24 16:10:39 +0300

    enabled admin interface

--- surveything/settings.py
+++ surveything/settings.py
@@ -36,8 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    #'django.contrib.messages',
-    #'django.contrib.staticfiles',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
