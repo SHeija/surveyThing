1a713de89d6f96a1c792a05ab6ef344754be97e5
CMS <sirkku.heija@gmail.com>
2019-05-02 13:29:00 +0300

    WIP

--- surveys/forms.py
+++ surveys/forms.py
@@ -1,7 +1,19 @@
 from django.forms import ModelForm
 from .models import Survey, Question
-
-class SurveyCreationForm(ModelForm):
-    class Meta:
+from betterforms.multiform import MultiModelForm
+    
+class SurveyForm(ModelForm):
+   class Meta:
         model = Survey
         fields = ['title', 'description']
+
+class QuestionForm(ModelForm):
+    class Meta:
+        model = Question
+        fields = ['question_text']
+
+class SurveyCreationForm(ModelForm):
+    form_classes = {
+        'survey': SurveyForm,
+        'question': QuestionForm,
+    }
