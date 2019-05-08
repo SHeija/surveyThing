4a88e369bb82874fc06c0cb3385f1a3b7ff99446
CMS <sirkku.heija@gmail.com>
2019-05-08 21:33:17 +0300

    attempts continue

--- surveything/settings.py
+++ surveything/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -91,16 +92,7 @@ WSGI_APPLICATION = 'surveything.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'surveything',
-        'USER': 'surveything_admin',
-        'PASSWORD': '1234admin',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
